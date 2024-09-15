5839b401add8d95f1be804c52453291ecf8d0b78
unknown <arya.krishna@somaiya.edu>
2024-09-15 17:55:53 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Home from './pages/Home/Home'
 import "./index.css";
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Home />
   </React.StrictMode>,
   document.getElementById('root')
 );
