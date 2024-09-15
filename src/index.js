8d50ba5398476dd576795e18cf97cea8a6e1eb91
Aaryaveerkrishna23 <86841659+Aaryaveerkrishna23@users.noreply.github.com>
2024-09-15 17:41:22 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Home from './Home';
+
 import "./index.css";
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Home />
   </React.StrictMode>,
   document.getElementById('root')
 );
